ac9c7da569624bae831fcb10aea71a151046e751
Don-drews <tsuchida2892@outlook.com>
2025-05-09 17:11:18 +0900

    不要なインポートの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
 import Login from "./components/Login";
 import CustomerList from "./components/CustomerList";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // return <div>{isLoggedIn ? <CustomerList /> : <Login />}</div>;
-
   return (
     <Routes>
       <Route path="/" element={<Login />} />
